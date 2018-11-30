acc1a80c91f6420b879612fb57afebb3ca247297
aarontbautista <aaronnolbautista@gmail.com>
2018-11-30 23:24:27 +0800

    landing page + cart

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'views')));
 
 //set port
 var port = process.env.PORT || 8080
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/views"));
 
 //routes
 app.get("/", function(req, res) {
-	res.render("index");
+	res.render('landingpage')
 })
+
+app.get('/cart', function(req, res) {
+  res.render('cart');
+});
 
 app.listen(port, function() {
 	console.log("app running");
